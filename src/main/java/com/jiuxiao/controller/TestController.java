b7b7abd84824a82b1639be109e6008176308872e
WuDaoJiuXiao <skg_xsn@126.com>
2022-05-26 08:19:33 +0800

    侧边栏、分类、标签、关于我等页面

--- src/main/java/com/jiuxiao/controller/TestController.java
+++ src/main/java/com/jiuxiao/controller/TestController.java
@@ -13,8 +13,51 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class TestController {
 
+    //博客首页
     @RequestMapping("/")
-    public String test(){
-        return "index";
+    public String test01(){
+        return "mainPage/index";
+    }
+
+    //博文内容页面
+    @RequestMapping("/cont")
+    public String test02(){
+        return "mainPage/blogContents";
+    }
+
+    //分类页面
+    @RequestMapping("/class")
+    public String test03(){
+        return "mainPage/classPage";
+    }
+
+    //标签页面
+    @RequestMapping("/tag")
+    public String test04(){
+        return "mainPage/tagsPage";
+    }
+
+    //杂谈页面
+    @RequestMapping("/game")
+    public String test05(){
+        return "mainPage/gamePage";
+    }
+
+    //归档页面
+    @RequestMapping("/file")
+    public String test06(){
+        return "mainPage/filePage";
+    }
+
+    //关于我页面
+    @RequestMapping("/info")
+    public String test07(){
+        return "mainPage/aboutMePage";
+    }
+
+    //友链页面
+    @RequestMapping("/link")
+    public String test08(){
+        return "mainPage/linkPage";
     }
 }
