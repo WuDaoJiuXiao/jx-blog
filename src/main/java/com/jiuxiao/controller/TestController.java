a88d3ebd24048d34fcb208785655305442f12f69
WuDaoJiuXiao <skg_xsn@126.com>
2022-05-28 20:46:15 +0800

    前端基础布局全部完成，新增博客管理、发布界面

--- src/main/java/com/jiuxiao/controller/TestController.java
+++ src/main/java/com/jiuxiao/controller/TestController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
- * 新建项目后测试环境
+ * 无后端时测试前端页面用
  *
  * @Author: 悟道九霄
  * @Date: 2022年05月23日 17:37
@@ -25,39 +25,59 @@ public class TestController {
         return "mainPage/blogContents";
     }
 
-    //分类页面
-    @RequestMapping("/class")
-    public String test03(){
-        return "mainPage/classPage";
-    }
-
     //标签页面
     @RequestMapping("/tag")
-    public String test04(){
+    public String test03(){
         return "mainPage/tagsPage";
-    }
-
-    //杂谈页面
-    @RequestMapping("/game")
-    public String test05(){
-        return "mainPage/gamePage";
     }
 
     //归档页面
     @RequestMapping("/file")
-    public String test06(){
+    public String test04(){
         return "mainPage/filePage";
     }
 
     //关于我页面
     @RequestMapping("/info")
-    public String test07(){
+    public String test05(){
         return "mainPage/aboutMePage";
     }
 
     //友链页面
     @RequestMapping("/link")
-    public String test08(){
+    public String test06(){
         return "mainPage/linkPage";
+    }
+
+    //------------ 后台页面 ---------------
+
+    //管理员登录页面
+    @RequestMapping("/admin/login")
+    public String test07(){
+        return "backend/adminLogin";
+    }
+
+    //后台博客管理
+    @RequestMapping("/admin/blog")
+    public String test08(){
+        return "backend/blogControl";
+    }
+
+    //发布博客
+    @RequestMapping("/admin/publish")
+    public String test09(){
+        return "backend/publish";
+    }
+
+    //后台标签管理
+    @RequestMapping("/admin/tags")
+    public String test10(){
+        return "backend/tagsControl";
+    }
+
+    //后台友链管理
+    @RequestMapping("/admin/link")
+    public String test11(){
+        return "backend/linkControl";
     }
 }
